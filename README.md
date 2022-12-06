075fcc6a4b082de043cbe2bcd32d98c39ba98629
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-12-06 20:38:35 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# NovaBot-MD
+The whatbot-MD todavía no está terminado bot el desarrollo✨
+
+<p align="center"> 
+  <a href="https://github.com/elrebelde21"><img src="http://readme-typing-svg.herokuapp.com?font=mono&size=17&duration=4000&color=F7B11B&center=falso&vCenter=falso&lines=The+ WhatBot-MD++%F0%9F%92%96;Gracias+por+visitar+este+repositorio.+%F0%9F%92%96" height="90px"
+</p>
+
+### `—◉ DUDAS SOBRE EL BOT?, CONTACTAME ✨`
+<a href="http://wa.me/5492266466080" target="blank"><img src="https://img.shields.io/badge/creador-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" /></a>
+<a href="http://wa.me/51935116539" target="blank"><img src="https://img.shields.io/badge/desarrollo¹-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" /></a>
+<a href="http://wa.me/5218992059110" target="blank"><img src="https://img.shields.io/badge/desarrollo²-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" /></a>
+
+
+[![blog](https://img.shields.io/badge/Grupo-25D366?style=for-the-badge&logo=whatsapp&logoColor=white 
+)](https://chat.whatsapp.com/Byyrc8RJnUkJw6vvUgsbDF)  <a href="https://chat.whatsapp.com/Byyrc8RJnUkJw6vvUgsbDF"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/WhatsApp_logo-color-vertical.svg/1200px-WhatsApp_logo-color-vertical.svg.png" height="29px"></a>
+---------
+
+<p align="hihg">   
+<a href="https://facebook.com/groups/721802642266362" target="_blank"> <img src="https://img.shields.io/badge/-grupo asistencia facebook-%23E4405F?style=for-the-badge&logo=facebook&logoColor=blue" target="_blank"></a> <img src="https://github.com/siegrin/siegrin/blob/main/Assets/Handshake.gif" height="30px">
+
+<p align="center">
+<img src="https://telegra.ph/file/fed1c9d94b37511e570d5.mp4" alt="" width="500"/>
+
+[![blog](https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white)
+](https://youtu.be/gh5NajZOTA8)  <img src="https://github.com/siegrin/siegrin/blob/main/Assets/powerup.gif" height="29px">
+
+<p align="center"> 
+  <a href="https://github.com/elrebelde21"><img src="http://readme-typing-svg.herokuapp.com?font=mono&size=17&duration=4000&color=F7B11B&center=falso&vCenter=falso&lines=pagina++%F0%9F%92%96;quiere+tener+un+bot+activo+24/7+adicnodes..+%F0%9F%92%96" height="90px"
+</p>
+
+<p align="hihg">   
+<a href="https://portal.acidicnodes.com" target="_blank"> <img src="https://img.shields.io/badge/-AcidicNodes-%23E4405F?style=for-the-badge&logo=acidicnodes&logoColor=black" target="_blank"></a> <img src="https://github.com/siegrin/siegrin/blob/main/Assets/Handshake.gif" height="30px">
+
+
+---------
+
+## 𝐀𝐜𝐭𝐢𝐯𝐚 𝐞𝐥 𝐭𝐞𝐫𝐦𝐮𝐱
+```bash
+> pkg install
+> pkg upgrade
+> pkg install git
+> pkg install ffmpeg && pkg install libwebp
+> pkg install nodejs
+> pkg install yarn
+> git clone https://github.com/elrebelde21/The-whatbot-MD
+> cd The-whatbot-MD
+> npm install
+> yarn
+> npm start
+```
+
+## 𝐀𝐜𝐭𝐢𝐯𝐚 𝐞𝐥 𝐡𝐞𝐫𝐨𝐤𝐮
+𝐏𝐚𝐫𝐚 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐝𝐞 𝐡𝐞𝐫𝐨𝐤𝐮
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/elrebelde21/The-whatbot-MD) <img src="https://c.tenor.com/OIc0cAei3dwAAAAd/gato-baile.gif" height="32px">
+### Añada lo siguente al Buildpack: <img src="https://cdn-0.emojis.wiki/emoji-pics/microsoft/backhand-index-pointing-down-microsoft.png" height="32px">
+```bash
+> heroku/nodejs
+```
+```bash
+> https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest.git
+```
+```bash
+> https://github.com/DuckyTeam/heroku-buildpack-imagemagick.git*
+```
+
+## 𝐀𝐜𝐭𝐢𝐯𝐚 𝐞𝐥 𝐖𝐢𝐧𝐝𝐨𝐰𝐬
+𝐏𝐚𝐫𝐚 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐝𝐞 𝐖𝐢𝐧𝐝𝐨𝐰𝐬
+
+* Descarga cualquiera de las versiones de FFmpeg disponibles haciendo clic en [di sini](https://www.gyan.dev/ffmpeg/builds/).
+* Extraer archivo ke `C:\` path.
+* Cambie el nombre de la carpeta extraída a `ffmpeg`.
+* Ejecute el símbolo del sistema como administrador..
+* Ejecute el siguiente comando::
+```cmd
+> setx /m PATH "C:\ffmpeg\bin;%PATH%"
+
+```
+## `𝐄𝐃𝐈𝐓𝐎𝐑 𝐘 𝐏𝐎𝐑𝐏𝐈𝐄𝐓𝐀𝐑𝐈𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓` 
+<a href="https://github.com/elrebelde21"><img src="https://github.com/elrebelde21.png" width="300" height="300" alt="elrebelde21"/></a>
+
+`The LoliBot-MD _ By el rebelde`
+
+## `𝐂𝐎𝐋𝐀𝐁𝐎𝐑𝐀𝐂𝐈𝐎𝐍` 
+[![Misaelsharkbot](https://github.com/Misaelsharkbot.png?size=100)](https://github.com/Misaelsharkbot)
+[![Skidy](https://github.com/Skidy89.png?size=100)](https://github.com/Skidy89)
+
+
+# 𝐀𝐠𝐫𝐚𝐜𝐢𝐦𝐢𝐞𝐧𝐭𝐨 
---
+++ config.js
@@ -0,0 +1,126 @@
+//=============『 owner 』================== //
+global.owner = ['5492266466080' , '51935116539'] 
+global.mods = [] 
+global.prems = [] 
+
+//=============『 Info Owner 』============== //
+global.nameowner = 'creador'
+global.numberowner = '5492266466080' , '51935116539'
+global.instagram = ''
+global.github = ''
+global.dana = '087892711054'
+global.pulsa = '085212023766'
+global.gopay = '087892711054'
+
+//=============『 Info Bot 』=================//
+global.namebot = 'NovaBot-MD'
+global.gc = 'bit.ly/AnyaBot-Group'
+global.web = 'https://github.com/Hyzerr' //conviértalo en su sitio web, puede vincular ig, vincular github, vincular yt, si el enlace es gc, la visualización volverá a ser diferente.
+global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
+
+//=======『 Pantalla y más 』============//
+global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //cambia a tu foto de bot
+global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //cambiar esto también
+global.lolkey = 'Papah-Chan' //asi que es facil remplazarlo, por ejemplo el apikey esta caducado :v
+global.zenzkey = 'BagasPrdn' //cambiar a su apikey si caduca
+global.wm = 'NovaBot-MD'
+global.watermark = wm
+global.wm2 = '                     「 NovaBot-MD⁩ 」'
+global.wm3 = 'superbot-𝙼𝙳'
+global.htki = '––––––『' 
+global.htka = '』––––––'
+global.media = 'https://telegra.ph/file/fed1c9d94b37511e570d5.mp4'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.wait = '_*espere unos minutos...*_'
+global.eror = '_*Error*_'
+global.benar = 'verdad ✅\n'
+global.salah = 'falso ❌\n'
+global.stiker_wait = 'Stiker'
+global.packname = 'toma tu sticker'
+global.author = '©NovaBot-MD'
+
+//=============『 Apikey 』================== //
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  amel: 'https://melcanz.com',
+    bx: 'https://bx-hunter.herokuapp.com',
+  dhnjing: 'https://dhnjing.xyz',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  zekais: 'http://zekais-api.herokuapp.com',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  erdwepe: 'https://erdwpe-api.herokuapp.com',
+  lolhuman: 'https://api.lolhuman.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://melcanz.com': 'elaina',
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.xteam.xyz': 'd37372311698ed1d',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://zekais-api.herokuapp.com': 'apikeymu',
+  'https://api.lolhuman.xyz': 'Deffbotz',
+}
+
+//=============『 JUEGOS | GAMES 』================== //
+global.multiplier = 69 // Cuanto más alto, más difícil subir de nivel
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      petFood: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+//===========『 No cambies 』================ //
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
